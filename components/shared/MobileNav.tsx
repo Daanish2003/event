a99b35eae7a16e81cc28731b7c367c01afddc746
Daanish Azim Sheikh <redchillies6786@gmail.com>
2024-01-08 23:06:07 +0530

    auth

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,44 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import NavItems from "./Navitems";
+
+  
+
+const MobileNav = () => {
+  return (
+    <nav className="md:hidden">
+       <Sheet>
+        <SheetTrigger className="align-middle">
+             <Image 
+              src="/assets/icons/menu.svg"
+              alt="menu"
+              width={24}
+              height={24}
+              className="cursor-pointer"
+             />
+             </SheetTrigger>
+        
+         <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+           <Image 
+           src="/assets/images/logo.svg"
+           alt="logo"
+           height={38}
+           width={128}
+           />
+           <Separator className="border border-grey-50"/>
+            <NavItems />
+         </SheetContent>
+       </Sheet>
+    </nav>
+  )
+}
+
+export default MobileNav
